37aa060e207b4fd3c41ffc57b7349bcb6c6395d3
HP <HP@DESKTOP-7MPP6VI>
2024-08-05 16:51:54 +0530

    phonepe payment added by teamlead

---
+++ gitapp01/src/main/java/com/nit/payment/PhonePe.java
@@ -0,0 +1,8 @@
+package com.nit.payment;
+
+public class PhonePe {
+	public String doUpiPayment() {
+		System.out.println("payment done through netbanking");
+		return "payment done through netbanking";
+	}
+}
