1c7f988afc00ec250d496bfe41f8478c21e3ebd8
HP <HP@DESKTOP-7MPP6VI>
2024-08-05 11:31:24 +0530

    dev1-added sum,sum1,mul and A.java

--- gitapp01/src/main/java/com/nit/service/Service.java
+++ gitapp01/src/main/java/com/nit/service/Service.java
@@ -1,5 +1,21 @@
 package com.nit.service;
 
 public class Service {
+	
+	public int sum(int x,int y) {
+		return x+y;
+	}
+	
+	public int sum1(int x,int y) {
+		return x+y;
+	}
+	
+	
+
+	public int mul(int x,int y) {
+		return x*y;
+	}
+	
+	 
 
 }
