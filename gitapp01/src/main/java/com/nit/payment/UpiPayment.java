5e2b65287e117c2a91f1495b2ee45327415482b4
HP <HP@DESKTOP-7MPP6VI>
2024-08-05 16:37:52 +0530

    new branch created by team lead

---
+++ gitapp01/src/main/java/com/nit/payment/UpiPayment.java
@@ -0,0 +1,10 @@
+package com.nit.payment;
+
+public class UpiPayment {
+	
+	public String doUpiPayment() {
+		System.out.println("payment done through upi");
+		return "payment done through upi";
+	}
+
+}
