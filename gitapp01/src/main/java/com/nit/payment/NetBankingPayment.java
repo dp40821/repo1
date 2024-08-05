b90e3f7b3255304bc486df532e6ab138da6193d3
HP <HP@DESKTOP-7MPP6VI>
2024-08-05 16:47:32 +0530

    netbanking added by dev

---
+++ gitapp01/src/main/java/com/nit/payment/NetBankingPayment.java
@@ -0,0 +1,8 @@
+package com.nit.payment;
+
+public class NetBankingPayment {
+	public String doUpiPayment() {
+		System.out.println("payment done through netbanking");
+		return "payment done through netbanking";
+	}
+}
